9f51f9b0ddf8085ec2aeee13a2298f19c3338521
Jose Montoya <jam01@protonmail.com>
2019-03-26 12:48:20 -0500

    Fixes agent test

--- src/test/java/io/opentracing/contrib/reactor/AgentTest.java
+++ src/test/java/io/opentracing/contrib/reactor/AgentTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.*;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+import io.opentracing.Span;
+import org.awaitility.Awaitility;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -11,15 +15,17 @@ import io.opentracing.Scope;
 import io.opentracing.contrib.specialagent.AgentRunner;
 import io.opentracing.mock.MockSpan;
 import io.opentracing.mock.MockTracer;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 /**
  * @author Jose Montoya
  */
 @RunWith(AgentRunner.class)
 public class AgentTest {
-
-	// #1030
+	
 	@Test
 	public void checkTraceIdFromSubscriberContext(MockTracer tracer) {
 		MockSpan initSpan = tracer.buildSpan("foo").start();
@@ -27,10 +33,12 @@ public class AgentTest {
 
 		try (Scope ws = tracer.scopeManager().activate(initSpan, true)) {
 			Mono.subscriberContext()
-					.map(context -> ((MockSpan) tracer.activeSpan()).context().spanId())
-					.doOnNext(spanInSubscriberContext::set).block();
+					.map(context -> ((MockSpan) context.get(Span.class)).context().spanId())
+					.doOnNext(spanInSubscriberContext::set)
+					.block();
 		}
 
 		assertEquals((long) spanInSubscriberContext.get(), initSpan.context().spanId()); // ok here
 	}
+
 }
